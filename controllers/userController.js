5323985f70011e32e9f0f87742a548ed5f47754c
ichson <122417410+ichson@users.noreply.github.com>
2023-01-11 16:46:44 -0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,30 @@
 const md5 = require("md5");
 const UserModel = require("../models/userSchema");
-const axios = require('axios');
 
-exports.registerUser = async (req, res) => {
-  
-    const existingUser = await UserModel.findOne({ email: req.body.email });
-    if (existingUser) {
+// Register Controller
+exports.registerUser = (req, res) => {
+  UserModel.find({ email: req.body.email }).exec(function (error, result) {
+    if (result.length > 0) {
       req.flash("message", "Email address already exists.");
       req.flash("messageType", "danger");
-      return res.redirect("/register");
-    
-    const user = new UserModel({
-      name: req.body.name,
-      email: req.body.email,
-      funds: 0,
-      password: req.body.password,
-    });
-    await user.save();
-    req.flash("message", "Your account has been successfully created.");
-    req.flash("messageType", "success");
-    res.redirect("/login");
-    
+      res.redirect("/register");
+    } else {
+      const user = new UserModel({
+        name: req.body.name,
+        email: req.body.email,
+        funds: 0,
+        password: md5(req.body.password)
+       
+      });
+
+      user.save(function (err, doc) {
+        req.flash("message", "Your account has been successfully created.");
+        req.flash("messageType", "success");
+        res.redirect("/login");
+      });
     }
-}
-
-
-    
-
+  });
+};
 // Exchange Controller
 
 
